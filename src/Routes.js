aceca496ffe47d089094343de2d4cd0c33f48661
Harsha Kumar <harzkr142@gmail.com>
2022-06-22 04:44:46 +0530

    Update auth redirects

--- src/Routes.js
+++ src/Routes.js
@@ -1,23 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
 import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Conversation from "./components/Conversation";
 
-const routes = () => {
-  const accessToken = localStorage.getItem("accessToken");
+function RequireAuth() {
+  let accessToken = localStorage.getItem("accessToken");
+  let location = useLocation();
 
+  if (!accessToken) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" state={{ from: location }} />;
+  }
+
+  return <></>;
+}
+
+
+const routes = () => {
   return (
     <Router>
       <Routes>
-        {accessToken ? (
-          <Route path="/login" element={<Login />} />
-        ) : (
-          <>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/conversation/:user" element={<Conversation />} />
-          </>
-        )}
+        <Route path="/login" element={<Login />} />
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/conversation/:user" element={<Conversation />} />
+        </Route>
       </Routes>
     </Router>
   );
