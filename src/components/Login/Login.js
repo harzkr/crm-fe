6539a924fcb0287b231cd77be31bd2a64019b6be
Harsha Kumar <harzkr142@gmail.com>
2022-06-22 04:57:10 +0530

    Update Login Form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { TextField, Button } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return(
-        <div>
-            Login Form
-        </div>
-    )
-}
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          id="email"
+          label="Email"
+          variant="filled"
+          {...register("email")}
+        />
+        <TextField
+          id="password"
+          label="Password"
+          variant="filled"
+          {...register("password")}
+        />
+
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
