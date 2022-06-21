51899dfb14780fe9285637cc8c47d451eb20fcd4
Harsha Kumar <harzkr142@gmail.com>
2022-06-21 23:29:21 +0530

    refactor routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import {
-    BrowserRouter as Router,
-    Route,
-  } from "react-router-dom";
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import Conversation from './components/Conversation';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import Conversation from "./components/Conversation";
 
 const Routes = () => {
-    return(
-        <Router>
-            <Route path='/login'>
-                <Login/>
-            </Route>
-            <Route path='/'>
-                <Dashboard/>
-            </Route>
-            <Route path='/conversation/:user'>
-                <Conversation/>
-            </Route>
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <div>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+        <Route path="/conversation/:user">
+          <Conversation />
+        </Route>
+      </div>
+    </Router>
+  );
+};
 
 export default Routes;
