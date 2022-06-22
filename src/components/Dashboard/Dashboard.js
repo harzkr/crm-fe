82093f3c88fff7a34fe73103f7ae9d49e8fbef34
Harsha Kumar <harzkr142@gmail.com>
2022-06-22 06:54:59 +0530

    Dashboard user updates

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,6 +2,10 @@ import { Typography } from "@mui/material";
 import React from "react";
 import "./styles.css";
 import { USERS } from "./mock";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
 
 const Dashboard = () => {
   return (
@@ -14,8 +18,19 @@ const Dashboard = () => {
       <div style={{display:'flex', justifyContent:'space-evenly', flexWrap:'wrap'}}>
         {USERS.map((user) => (
           <div key={user.email} className="user">
-            <Typography>{user.name}</Typography>
-            <Typography>{user.email}</Typography>
+            <Card>
+            <CardContent>
+                <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+                    {user.name}
+                </Typography>
+                <Typography sx={{fontSize:14}}>
+                    {user.email}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">CHAT</Button>
+            </CardActions>
+            </Card>
           </div>
         ))}
       </div>
