b87026c44f755db02ec02a47d9f2cfd1ebd41a99
Harsha Kumar <harzkr142@gmail.com>
2022-06-22 06:27:17 +0530

    Update dashboard styles and margins

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import { Typography } from "@mui/material";
+import React from "react";
+import "./styles.css";
 
 const Dashboard = () => {
-    return(
-        <div>
-            Dashboard Form
-        </div>
-    )
-}
+  return (
+    <div className="outer">
+      <div style={{marginTop:24}}>
+        <Typography variant="h3" className="title">
+          Welcome!
+        </Typography>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
 export default Dashboard;
