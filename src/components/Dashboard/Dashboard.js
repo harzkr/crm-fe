81d248a475bf96804939b7cd826468579648fe34
Harsha Kumar <harzkr142@gmail.com>
2022-06-22 06:55:34 +0530

    refactor dash page

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,10 +2,10 @@ import { Typography } from "@mui/material";
 import React from "react";
 import "./styles.css";
 import { USERS } from "./mock";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
 
 const Dashboard = () => {
   return (
@@ -15,21 +15,29 @@ const Dashboard = () => {
           Welcome!
         </Typography>
       </div>
-      <div style={{display:'flex', justifyContent:'space-evenly', flexWrap:'wrap'}}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          flexWrap: "wrap",
+        }}
+      >
         {USERS.map((user) => (
           <div key={user.email} className="user">
             <Card>
-            <CardContent>
-                <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
-                    {user.name}
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 16 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  {user.name}
                 </Typography>
-                <Typography sx={{fontSize:14}}>
-                    {user.email}
-                </Typography>
-            </CardContent>
-            <CardActions>
+                <Typography sx={{ fontSize: 14 }}>{user.email}</Typography>
+              </CardContent>
+              <CardActions>
                 <Button size="small">CHAT</Button>
-            </CardActions>
+              </CardActions>
             </Card>
           </div>
         ))}
