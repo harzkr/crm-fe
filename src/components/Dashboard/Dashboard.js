82dd6bb788b15b8a81e74b7810f0dc026e43902e
Harsha Kumar <harzkr142@gmail.com>
2022-06-22 06:40:36 +0530

    Add mock data

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,24 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import "./styles.css";
+import { USERS } from "./mock";
 
 const Dashboard = () => {
   return (
     <div className="outer">
-      <div style={{marginTop:24}}>
+      <div style={{ marginTop: 24 }}>
         <Typography variant="h3" className="title">
           Welcome!
         </Typography>
       </div>
-      <div></div>
+      <div style={{display:'flex', justifyContent:'space-evenly', flexWrap:'wrap'}}>
+        {USERS.map((user) => (
+          <div key={user.email} className="user">
+            <Typography>{user.name}</Typography>
+            <Typography>{user.email}</Typography>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/components/Dashboard/mock.js
@@ -0,0 +1,16 @@
+import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker/locale/de';
+
+export const USERS = [];
+
+export function createRandomUser() {
+  return {
+    name: faker.name.firstName() + " " + faker.name.lastName(),
+    email: faker.internet.email(),
+    registeredAt: faker.date.past(),
+  };
+}
+
+Array.from({ length: 20 }).forEach(() => {
+  USERS.push(createRandomUser());
+});
