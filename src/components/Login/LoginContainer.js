b617f6a53012df98f915c90053e3a1b896ff5323
Harsha Kumar <harzkr142@gmail.com>
2022-06-23 07:16:18 +0530

    Update API calls and add axios

--- src/components/Login/LoginContainer.js
+++ src/components/Login/LoginContainer.js
@@ -5,17 +5,19 @@ import Login from './Login';
 const LoginContainer = () => {
     const logIn = async (data) => {
         console.log('passing the data',data);
-        const response = await fetch('/auth/login', {
+        const response = await fetch('/v1/auth/login', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(data)
+        }).catch(err => {
+            console.log(err,'seeing explicit err');
         })
 
-        if (!response.ok) {
+        if (response.status !== 200) {
             const message = `An error has occured: ${response.status}`;
-            throw new Error(message);
+            console.log(response,'check check')
         }
 
         const userData = response.json();
@@ -29,6 +31,7 @@ const LoginContainer = () => {
         mutate,
     }
 
+    console.log(error,'check in react query');
     return <Login {..._props}/>
 }
 
