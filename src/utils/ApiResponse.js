25b2039887e4a23a1d21f11d8666775ab3b894fa
Harsha Kumar <harzkr142@gmail.com>
2022-06-24 02:09:56 +0530

    Update access token availability call

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -2,9 +2,17 @@ import axios from "axios";
 
 export const ApiResponse = (method, url, body) => {
   return new Promise((resolve, reject) => {
+    const config = {};
+
+    if(localStorage.getItem("accessToken")){
+      config.headers = {
+        "Authorization": `Bearer ${localStorage.getItem("accessToken")}`,
+      }
+    }
+
     if (method === "post") {
       axios
-        .post(url, body)
+        .post(url, body, config)
         .then((response) => {
           resolve(response);
         })
@@ -13,7 +21,7 @@ export const ApiResponse = (method, url, body) => {
         });
     } else {
       axios
-        .get(url)
+        .get(url, config)
         .then((response) => {
           resolve(response);
         })
