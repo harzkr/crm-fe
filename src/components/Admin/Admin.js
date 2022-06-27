e769e7a0f27b5a49b78392e106721c420ca3abcf
Harsha Kumar <harzkr142@gmail.com>
2022-06-27 08:24:02 +0530

    refactor changes

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import './styles.css';
 
 const Admin = () => {
-    return <div>Admin</div>;
-}
+  return (
+    <div>
+      <div className="pageTitle">
+        <Typography variant="h4">Admin</Typography>
+      </div>
+      <div>
+      </div>
+    </div>
+  );
+};
 
 export default Admin;
