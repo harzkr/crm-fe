ffe2f76c334133c7b2bf7323a233e522240b3243
Harsha Kumar <harzkr142@gmail.com>
2022-06-30 11:26:48 +0530

    Add paginated load

--- src/components/Admin/AdminContainer.js
+++ src/components/Admin/AdminContainer.js
@@ -1,7 +1,24 @@
 import React from "react";
 import Admin from "./Admin";
+import { useQuery } from "react-query";
+import { ApiResponse } from "../../utils/ApiResponse";
 
 const AdminContainer = () => {
+    const [page, setPage] = React.useState(1);
+
+    const generalDataUsers = async (page) => {
+        try {
+            const response = await ApiResponse('get', 'v1/users/general-data-users', {
+                params: { page: page },
+            });
+            return response;
+        } catch (err) {
+            console.log(err.data.message);
+        }
+    }
+
+    const { data, isLoading, error } = useQuery(['generalDataUsers', page], () => generalDataUsers(page), { keepPreviousData : true });
+
     return <Admin />;
 }
 
